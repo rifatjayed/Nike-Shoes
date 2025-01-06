1416cf57297ceb65138d47a4e5f68effef804f86
Rifat Jayed <rifat.jayed2000@gmail.com>
2025-01-07 00:50:23 +0600

    context api done

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,5 +1,6 @@
 import React, { createContext, useState } from "react";
 import all_products from "../Data/all_products";
+import all_product from "../Data/all_products";
 
 export const ShopContext = createContext(null);
 
@@ -21,7 +22,44 @@ const ShopContextProvider = () => {
   const removeFormCart = (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
   };
-  return <div></div>;
+
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = all_product.find(
+          (product) => product.id === Number(item)
+        );
+        totalAmount += cartItems[item] * itemInfo.new_price;
+      }
+    }
+    return totalAmount;
+  };
+
+  const getTotalCartItems = () => {
+    let totalItem = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        totalItem += cartItems[item];
+      }
+    }
+    return totalItem;
+  };
+
+  const contextValue = {
+    all_product,
+    cartItems,
+    addToCart,
+    removeFormCart,
+    getTotalCartAmount,
+    getTotalCartItems,
+  };
+
+  return (
+    <ShopContext.Provider value={contextValue}>
+      {props.children}
+    </ShopContext.Provider>
+  );
 };
 
 export default ShopContextProvider;
